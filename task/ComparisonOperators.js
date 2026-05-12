6a5a4a4d0822814e003cd3f3199133e8850052ad
sandhiyarajendran97 <sandhiyarajendran.97@outlook.com>
2026-05-12 23:01:10 +0530

    ComparisonOperators.js

---
+++ task/ComparisonOperators.js
@@ -0,0 +1,18 @@
+function comparison() {
+    let a = 10;
+    let b = 20;
+    let c = 30;
+
+    console.log(a > b);
+    console.log(a < b);
+    console.log(a >= b);
+    console.log(a <= b);
+
+
+}
+comparison();
+
+let marks = 24;
+
+let result = (marks >= 35) ? "pass" : "fail";
+console.log(result);
