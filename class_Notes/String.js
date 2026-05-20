cb1d580cf90a0738d938e59ce296a3da375f4415
sandhiyarajendran97 <sandhiyarajendran.97@outlook.com>
2026-05-20 21:57:25 +0530

    String

---
+++ class_Notes/String.js
@@ -0,0 +1,31 @@
+let a="javascript\n is a programming \tlanguage";
+let b ='hello playwright';
+let c =`playwright
+is a automation tool"old is gold" cant't! ${a}`
+
+//console.log(c);
+
+let str="welcome to chennai" //length starts from 1 -index starts from 0
+
+console.log(str.length);
+console.log(str.toUpperCase());
+console.log(str.toLowerCase());
+console.log(str.charAt(8));
+console.log(str.indexOf('e'));
+console.log(str.lastIndexOf('e'));
+console.log(str.includes("to"));
+console.log(str.startsWith("wel"));
+console.log(str.endsWith("chennai"));
+console.log(str.slice(0,7));
+console.log(str.slice(-7));
+console.log(str.replaceAll("e","*"));
+console.log(str.split(" "));
+
+//banana =b@n@n@
+//each char split
+
+let z="  hi   hello   ";
+console.log(z);
+console.log(z.trim());
+
+console.log(str.concat(z));
