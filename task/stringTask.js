1496fd569743d2eeef7335ad6bb8df209401409b
sandhiyarajendran97 <sandhiyarajendran.97@outlook.com>
2026-05-26 22:34:08 +0530

    string Task

---
+++ task/stringTask.js
@@ -0,0 +1,44 @@
+let fruit = "banana";
+console.log(fruit.replaceAll("a","@"));
+let fruits = fruit.split("");
+console.log(fruits);
+let sub="playwright";
+console.log(sub.slice(7));
+let str = "hi i am javascript";
+let count = str.split(" ").length;
+console.log(count);
+let replace ="chennai city center";
+console.log(replace.replaceAll("c","C"));
+
+let are = [];
+are.push("10,20,30");
+console.log(are);
+let art=[10,20,30];
+art.pop();
+console.log(art);
+art.unshift(5);
+console.log(art);
+art.shift();
+console.log(art);
+let name = "abinaya";
+console.log(name.split("").reverse().join(""));
+
+let palin = "moon";
+
+let drome= palin.split("").reverse().join("");
+
+if (palin==drome){
+    console.log(palin +  "is a palindrome");
+
+}else{
+    console.log(palin +  " is not a palindrome");
+}
+
+let fruit ="Apple";
+let numb= fruit[0].tolowercase ();
+
+
+
+
+
+
