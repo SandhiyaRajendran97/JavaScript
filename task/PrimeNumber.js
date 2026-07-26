71ecedc26bac46b9aefb60137b8e5f012d5bc69a
sandhiyarajendran97 <sandhiyarajendran.97@outlook.com>
2026-07-26 21:29:01 +0530

    PrimeNumber

--- task/PrimeNumber.js
+++ task/PrimeNumber.js
@@ -13,3 +13,30 @@ if (prime && num > 1) {
 } else {
     console.log("Not Prime");
 }
+
+let arr = [1, 2, 3, 2, 2, 4];
+
+let count = 0;
+
+for (let num of arr) {
+    if (num === 2) {
+        count++;
+    }
+}
+
+console.log(count);
+
+let n = 10;
+let first = 0;
+let second = 1;
+
+console.log(first);
+console.log(second);
+
+for (let i = 3; i <= n; i++) {
+    let next = first + second;
+    console.log(next);
+
+    first = second;
+    second = next;
+}
