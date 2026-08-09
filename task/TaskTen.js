4fc3578088ebd46b92bb488ff32929e79978247b
sandhiyarajendran97 <sandhiyarajendran.97@outlook.com>
2026-08-09 22:41:34 +0530

    Tenth Task

--- task/TaskTen.js
+++ task/TaskTen.js
@@ -239,3 +239,29 @@ console.log("Descending :", values);
 
 console.log("\nProgram Executed Successfully.");
 
+
+let arr = [10, 25, 5, 40, 15];
+
+let largest = arr[0];
+
+for (let i = 1; i < arr.length; i++) {
+    if (arr[i] > largest) {
+        largest = arr[i];
+    }
+}
+
+console.log(largest);
+
+let arr = [10, 20, 10, 30, 20, 40];
+
+let duplicates = [];
+
+for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+        if (arr[i] === arr[j] && !duplicates.includes(arr[i])) {
+            duplicates.push(arr[i]);
+        }
+    }
+}
+
+console.log(duplicates);
