0e6adcf7f9d9fbddb28cad9f80f6b2ff0de18f3b
sandhiyarajendran97 <sandhiyarajendran.97@outlook.com>
2026-09-03 21:32:28 +0530

    new code added

--- task/TaskTen.js
+++ task/TaskTen.js
@@ -693,3 +693,18 @@ let employe = [
     }
 ];
 
+function counter() {
+    let count = 0;
+
+    return function () {
+        count++;
+        return count;
+    };
+}
+
+const c = counter();
+
+console.log(c());
+console.log(c());
+console.log(c());
+
