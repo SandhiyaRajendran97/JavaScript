4a7ad3809b658b27fd582408422563d2314b5b2d
sandhiyarajendran97 <sandhiyarajendran.97@outlook.com>
2026-09-12 22:39:50 +0530

    PrimeNumber

--- task/TaskTen.js
+++ task/TaskTen.js
@@ -804,5 +804,23 @@ for (let i = 1; i <= 10; i++) {
     console.log(number + " x " + i + " = " + (number * i));
 }
 
+for (let number = 2; number <= 50; number++) {
+
+    let isPrime = true;
+
+    for (let i = 2; i < number; i++) {
+
+        if (number % i === 0) {
+            isPrime = false;
+            break;
+        }
+
+    }
+
+    if (isPrime) {
+        console.log(number);
+    }
+}
+
 
 
