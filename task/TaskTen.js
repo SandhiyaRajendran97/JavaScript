bde311a7ae4ab2d42508c2b149517fc6b6d6d2a6
sandhiyarajendran97 <sandhiyarajendran.97@outlook.com>
2026-09-23 00:10:40 +0530

    Prime numbers

--- task/TaskTen.js
+++ task/TaskTen.js
@@ -896,4 +896,21 @@ if (number % i === 0) {
     // not prime
 }
 
+for (let number = 2; number <= 50; number++) {
+
+    let isPrime = true;
+
+    for (let i = 2; i < number; i++) {
+
+        if (number % i === 0) {
+            isPrime = false;
+            break;
+        }
+
+    }
+
+    if (isPrime) {
+        console.log(number);
+    }
+}
 
